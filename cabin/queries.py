c55599158fc1d846df38463bf8f08f88ba5a6ef6
mohammadmirmoosavi <mohammadmirmoosavi@gmail.com>
2023-09-02 19:20:30 +0330

    feat(first query) write first query to get all of the payments that a driver get

--- cabin/queries.py
+++ cabin/queries.py
@@ -11,7 +11,29 @@ def query_0(x):
 
 
 def query_1(x):
-    q = 'your query here'
+    # this is only help for model relations
+    # level1 :Payment (ride) ->
+    # level2 :Ride (car) ->
+    # level3 :Car (owner) ->
+    # level4 :Driver
+
+    # we should aggregate payments of the ride that have the exact car that belong to specific owner (driver)
+    # we use objects aggregate with using Sum , When and Case django expressions to
+    # find every driver ( we named it Conditional Sum)
+    q = Payment.objects.aggregate(
+        payment_sum=Sum(
+            Case(
+                When(
+                    # we use id because of the foreign key is indexed on db and has better performance
+                    # in comparsion to ride__car__owner ( this snippet need compare every objects)
+                    # but the way compare indexed id and have better performance
+                    ride__car__owner__id=x, then='amount'
+                ),
+                default=None,
+                output_field=IntegerField()
+            )
+        )
+    )
     return q
 
 
